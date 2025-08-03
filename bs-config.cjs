cc3cf85a9a6d834c1c70d7bf06126d462d59c163
coreypnorris <coreypnorris@gmail.com>
2025-08-03 12:17:44 -0500

    Add bs-config.cjs file.

---
+++ bs-config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    server: {
+        baseDir: "./",
+        index: "index.html"
+    },
+    files: [
+        {
+            match: ["dist/**/*.js", "dist/**/*.js.map"],
+            fn: function (event, file) {
+                console.log(`File changed: ${file}`);
+                this.reload();
+            }
+        },
+        "css/**/*.css",
+        "*.html"
+    ],
+    port: 8080,
+    open: true,
+    notify: false,
+    reloadDelay: 0,
+    reloadDebounce: 1000,
+    watchEvents: ['change', 'add'],
+    watchOptions: {
+        ignoreInitial: true,
+        ignored: ['node_modules/**', '.git/**']
+    }
+};
